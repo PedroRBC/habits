ccb4acb06db6b4f20b635628d0e1df039dc16a43
PedroRBC <pedrinhorbc@gmail.com>
2024-04-12 02:18:44 +0000

    chore: Showing Last Days of Habit's
    Fix only showing if has some one Habit Make.

--- web/src/components/summary-table.tsx
+++ web/src/components/summary-table.tsx
@@ -36,23 +36,22 @@ export function SummaryTable({ summary }: { summary: Summary }) {
       </div>
 
       <div className="grid grid-cols-7 lg:grid-cols-none lg:grid-rows-7 grid-flow-row lg:grid-flow-col gap-3">
-        {summary.length > 0 &&
-          summaryDates.map((date, i) => {
-            const dayInSummary = summary.find((day) => {
-              return dayjs(date).isSame(day.date, "day");
-            });
-            if (useReducedRange && i <= 7 * 8 - 1) {
-              return null;
-            }
-            return (
-              <HabitDay
-                key={date.toString()}
-                amount={dayInSummary?.amount}
-                defaultCompleted={dayInSummary?.completed}
-                date={date}
-              />
-            );
-          })}
+        {summaryDates.map((date, i) => {
+          const dayInSummary = summary.find((day) => {
+            return dayjs(date).isSame(day.date, "day");
+          });
+          if (useReducedRange && i <= 7 * 8 - 1) {
+            return null;
+          }
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={dayInSummary?.amount}
+              defaultCompleted={dayInSummary?.completed}
+              date={date}
+            />
+          );
+        })}
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map((_, i) => {
             return (
