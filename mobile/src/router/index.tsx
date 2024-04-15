550e03b77c651de0076f2e4d32fe114e1b6512c5
PedroRBC <pedrinhorbc@gmail.com>
2024-04-15 01:47:56 -0300

    chore: remove unused Functions

--- mobile/src/router/index.tsx
+++ mobile/src/router/index.tsx
@@ -1,15 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { useContext } from "react";
+import React from "react";
 
 import { AppRoutes } from "./app.routes";
 
-import { StoreContext } from "@/contexts/store";
-
 export function Routes() {
-  const { status } = useContext(StoreContext);
-  if (status === "loading") {
-    return null;
-  }
   return (
     <NavigationContainer>
       <AppRoutes />
