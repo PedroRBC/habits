8bca029b625c2092ea46a7f65b916df373d84c6b
PedroRBC <pedrinhorbc@gmail.com>
2024-04-17 11:19:44 -0300

    chore: Recreate Login Ui

--- mobile/src/screens/Login.tsx
+++ mobile/src/screens/Login.tsx
@@ -1,15 +1,12 @@
 import { openAuthSessionAsync } from "expo-web-browser";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+import { Button } from "@/components/ui/button";
 import { ApiUrl } from "@/contexts/store";
 
 export function Login() {
   const inset = useSafeAreaInsets();
-
-  const handlePressLogin = async () => {
-    openAuthSessionAsync(`${ApiUrl}/auth/google?redirectApp=true`);
-  };
   return (
     <View
       className="flex-1 flex justify-around bg-background"
@@ -21,14 +18,25 @@ export function Login() {
         <Text className="text-6xl font-bold text-foreground">Habits</Text>
         <Text className="text-5xl font-bold text-foreground">Tracker</Text>
       </View>
-      <View className="items-center">
-        <TouchableOpacity
-          onPress={handlePressLogin}
-          activeOpacity={0.8}
-          className="bg-secondary p-2 rounded-md mt-2"
-        >
-          <Text className="text-foreground text-lg">Entrar com Google</Text>
-        </TouchableOpacity>
+      <View className="justify-center items-center">
+        <View className="p-4 native:pb-24 max-w-md gap-6">
+          <View className="gap-1">
+            <Text className="text-muted-foreground text-base text-center">
+              Enter with your account to start tracking your habits
+            </Text>
+          </View>
+          <Button
+            variant="outline"
+            size="lg"
+            className="rounded-md"
+            onPress={() =>
+              openAuthSessionAsync(`${ApiUrl}/auth/google?redirectApp=true`)
+            }
+          >
+            <Text className="text-foreground text-lg">Google</Text>
+          </Button>
+          <View />
+        </View>
       </View>
     </View>
   );
